e5f9df2ff83a7a4ccdcbf6e3f28ab44359072c14
onapoli <onapoli@protonmail.com>
2022-03-30 11:38:26 +0200

    Add test for Config class

---
+++ tests/config.cpp
@@ -0,0 +1,24 @@
+#include "../includes/Config.hpp"
+
+int main(int argc, char **argv)
+{
+  Config  conf;
+
+  if (argc > 2)
+  {
+    std::cout << "Too many arguments. Max. one argument for config file path.";
+    std::cout << std::endl;
+    return (1);
+  }
+  if (argc == 2)
+  {
+    conf.setPath(argv[1]);
+    //Only check validity of user provided config file
+    if (!conf.isValid())
+    {
+      std::cout << "Invalid config file." << std::endl;
+      return (1);
+    }
+  }
+  return (0);
+}
