058545e0a3bc80995f984f75227e793023170ca9
onapoli <onapoli@protonmail.com>
2022-03-31 13:23:00 +0200

    Add Config data struts and validation methods

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -1,15 +1,55 @@
 #pragma once
 
+#include <map>
+#include <vector>
+#include <stack>
+
 #include "webserv.hpp"
+
+struct LocationConfig
+{
+	std::string	root;
+	std::string	methods;
+	std::string	redirection;
+	bool				dir_list;
+	std::string	default_file;
+	std::string	upload_dir;
+
+	LocationConfig(void);
+};
+
+
+struct	ServerConfig
+{
+	unsigned int													port; // max. port number 65535
+	std::string														host;
+	std::string														server_name;
+	std::string														not_found_page;
+	unsigned int													max_body_size;
+	//map Key = location url, map Value = LocationConfig struct
+	std::map<std::string, LocationConfig>	location;
+
+	ServerConfig(void);
+};
 
 class	Config
 {
 	private:
 		//Config file path
-		std::string					_path;
+		std::string														_path;
+		//Map data structure to store different server configs
+		std::map< std::string, ServerConfig >	_serverConfig;
 
 		bool								_validPath(void) const;
-		bool								_validFile(void) const;
+		void								_tokenizeLine(std::string & line,
+													std::vector<std::string> & tokens);
+		bool								_process_braces(char brace, std::size_t & pos,
+													std::stack<std::pair<char, std::string>> & state);
+		bool								_process_brackets(char bracket, std::size_t & pos,
+													std::stack<std::pair<char, std::string>> & state);
+		bool								_getConfigData(std::vector<std::string> const & tokens);
+		bool								_checkMinConfig(void) const;
+		bool								_validFile(void);
 
 	public:
 		Config(void);
@@ -18,5 +58,5 @@ class	Config
 		std::string const &	getPath(void) const;
 		void								setPath(std::string const & path);
 
-		bool								isValid(void) const;
+		bool								isValid(void);
 };
--- sources/Config.cpp
+++ sources/Config.cpp
@@ -21,7 +21,7 @@ void  Config::setPath(std::string const & path)
   return ;
 }
 
-bool  Config::isValid(void) const
+bool  Config::isValid(void)
 {
   if (this->_path == ""
       || !this->_validPath()
@@ -44,15 +44,128 @@ bool  Config::_validPath(void) const
       || file_type_pos + 5 != this->_path.length() )
       return (false);
   //Check file exists
-  file.open(this->_path.c_str());
+  file.open(this->_path.c_str()); //C++98 ifstream doesn't accept std::string
   if (!file.is_open())
     return (false);
   file.close();
   return (true);
 }
 
-bool  Config::_validFile(void) const
+bool  Config::_process_braces(char brace, std::size_t & pos,
+  std::stack<std::pair<char, std::string>> & state)
 {
-  //PENDING...
+  if (brace == '{')
+  {
+    if (state.empty()) //Main brackets
+      state.push(std::pair<char, std::string>('{', ""));
+    else if (state.top().first == ',') //Next of multi_server or multi_location
+    {
+      state.pop();
+      state.push(std::pair<char, std::string>('{', state.top().second));
+    }
+    else if (state.top().first == '[') //First of multi_server or multi_location
+      state.push(std::pair<char, std::string>('{', state.top().second));
+    else if (!state.top().first
+              && (state.top().second == "server"
+              || state.top().second == "location"))
+      state.top().first = '{';
+    else
+      return (false);
+  }
+  else
+  {
+    if (state.top().first != '{')
+      return (false);
+    state.push(std::pair<char, std::string>('}', state.top().second));
+  }
+  pos += 1;
   return (true);
+}
+
+bool  Config::_getConfigData(std::vector<std::string> const & tokens)
+{
+  std::stack<std::pair<char, std::string>>  state;
+  std::vector<std::string>::const_iterator  it;
+  std::size_t                               pos;
+  bool                                      valid;
+
+  if (tokens.empty())
+    return (false);
+  for (it = tokens.begin(); it != tokens.end(); ++it)
+  {
+    pos = 0;
+    while ((*it)[pos])
+    {
+      //pos gets updated inside process methods
+      if ((*it)[pos] == '{' || (*it)[pos] == '}')
+        valid = this->_process_braces((*it)[pos], pos, state);
+      else if ((*it)[pos] == '[' || (*it)[pos] == ']')
+        valid = this->_process_brackets((*it)[pos], pos, state);
+      else if ((*it)[pos] == '"' || isdigit((*it)[pos]))
+      {}
+      else if ((*it)[pos] == ',')
+      {}
+      else if ((*it)[pos] == ':')
+      {}
+      else
+        valid = false;
+      if (!valid)
+        return (false);
+    }
+  }
+  return (true);
+}
+
+void  Config::_tokenizeLine(std::string & line,
+        std::vector<std::string> & tokens)
+{
+  std::stringstream line_stream;
+  std::string       token;
+
+  //Trim left and right whitespace
+  line.erase(0, line.find_first_not_of(" \n\r\t"));                                                                                               
+  line.erase(line.find_last_not_of(" \n\r\t")+1);
+  //Pass line to stringstream
+  line_stream << line;
+  //Get substrings delimited by spaces from stringstream
+  while (std::getline(line_stream, token, ' '))
+  {
+    //Prevents adding empty string when multiple spaces are found between tokens
+    if (token != "")
+      tokens.push_back(token);
+  }
+  return ;
+}
+
+bool  Config::_validFile(void)
+{
+  std::ifstream             file(this->_path.c_str());
+  std::string               line;
+  std::vector<std::string>  tokens;
+  bool                      valid;
+
+  //Tokenize all json file lines
+  while (std::getline(file, line))
+    this->_tokenizeLine(line, tokens);
+  //Returns false if bad syntax is encountered
+  valid = this->_getConfigData(tokens);
+  //Check if config file has minimum data for the server to work
+  if (valid && !this->_checkMinConfig())
+    valid = false;
+  file.close();
+  return (valid);
+}
+
+//STRUCTURE DEFAULT CONSTRUCTORS
+
+ServerConfig::ServerConfig(void) : port(0), host(""), server_name(""),
+  not_found_page(""), max_body_size(0)
+{
+  return ;
+}
+
+LocationConfig::LocationConfig(void) : root(""), methods(""),
+  redirection(""), dir_list(false), default_file(""), upload_dir("")
+{
+  return ;
 }
