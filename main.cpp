1306bc2e1d651395dcfacfd33fd5a73641311192
onapoli <onapoli@protonmail.com>
2022-04-21 14:31:54 +0200

    Main source file is now used to start the server

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 
 int		main(int argc, char **argv)
 {
-	
+  Config  config;
+	Server  server;
+
+  if (argc != 2)
+  {
+    std::cout << "Need config file path as argument. Nothing else.";
+    std::cout << std::endl;
+    return (1);
+  }
+  config.setPath(argv[1]);
+  if (!config.parseFile())
+  {
+    std::cout << "Invalid config file." << std::endl;
+    return (1);
+  }
+  if (!server.prepare(config.getConfig()))
+    return (1);
+  if (!server.start())
+    return (1);
+  return (0);
 }
