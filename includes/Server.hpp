b354908084d156233037dd5d49ae94ce72eda762
onapoli <onapoli@protonmail.com>
2022-04-21 14:29:08 +0200

    Server: Add multiserver and location functionality

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -8,26 +8,43 @@
 
 #include "webserv.hpp"
 
-#include <vector> // quitar
+#include <map>
 #include <algorithm>
 
-#define PORT 8080
+struct	ServerConfig;
+struct	LocationConfig;
+
 #define MAX_REQUEST 5 //TRY WITH 10?
+
+struct	ConnectionData
+{
+	std::vector<ServerConfig const *> *	portConfigs;
+	std::size_t													serverIndex;
+	std::size_t													locationIndex;
+	std::string													dataIn;
+	std::string													dataOut;
+};
 
 class	Server
 {
 	private:
 
-		std::vector<int>		_sockets;
-		struct sockaddr_in	_address;
-		int									_addrlen;
-		struct pollfd *			_connections;
-		std::size_t					_connLen;
-		std::size_t					_connCap;
+		std::map<int, std::vector<ServerConfig const *> >		_listeningSockets;
+		std::map<int, ConnectionData >											_connectionSockets;
+		struct pollfd *																			_connections;
+		std::size_t																					_connLen;
+		std::size_t																					_connCap;
 
+		bool	_initSocket(int & sock, std::size_t const port);
 		void	_removeConnection(std::size_t index);
-		bool	_sendData(int socket, std::string & data);
-		bool	_receiveData(int socket, std::string & data);
+		bool	_sendData(int socket);
+		void	_getResponse(std::string & data, ConnectionData const & conn) const;
+		void  _matchLocation(std::vector<LocationConfig> const & servers,
+												std::size_t & index, std::string const & reqUri);
+		void  _matchServer(std::vector<ServerConfig const *> & servers,
+												std::size_t & index, std::string const & reqHost);
+		void	_matchConfig(int socket);
+		bool	_receiveData(int socket);
 		void	_increaseConnCap(void);
 		bool	_acceptConn(int	socket);
 		bool	_handleEvent(std::size_t index);
@@ -38,5 +55,5 @@ class	Server
 		~Server();
 
 		bool	start(void);
-		bool	prepare(void);
+		bool	prepare(std::vector<ServerConfig> const & config);
 };
--- sources/Server.cpp
+++ sources/Server.cpp
@@ -1,11 +1,9 @@
 #include "Server.hpp"
 
-Server::Server(void) : _addrlen(sizeof(_address)), _connCap(100)
+Server::Server(void) : _connLen(0), _connCap(100)
 {
-  // Call Config class getConfig to fill _sockets
-  this->_sockets.push_back(0);
   this->_connections = new struct pollfd [this->_connCap];
-  this->_connLen = this->_sockets.size();
+  return ;
 }
 
 Server::~Server(void)
@@ -22,16 +20,10 @@ Server::~Server(void)
   delete[] this->_connections;
 }
 
-/*
-**  Obtain one listening socket per ServerConfig.
-**
-**  This function should be called for each listening socket.
-*/
-
-bool  Server::prepare(void)
+bool  Server::_initSocket(int & sock, std::size_t const port)
 {
-  int & sock = this->_sockets[0]; // PENDING loop implementation
-  int verdad = 1;
+  struct sockaddr_in  address;
+  int                 verdad = 1;
 
   //	Opening socket
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -56,15 +48,15 @@ bool  Server::prepare(void)
   }
 
   //	Setting address struct
-	this->_address.sin_family = AF_INET;
-	this->_address.sin_addr.s_addr = INADDR_ANY;
-	this->_address.sin_port = htons( PORT );
-  for (int i = 0; i < (int)sizeof(this->_address.sin_zero); i++)
-		this->_address.sin_zero[i] = 0;
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
+  for (int i = 0; i < (int)sizeof(address.sin_zero); i++)
+		address.sin_zero[i] = 0;
 
   //	Binding socket
-	if (bind(sock, (struct sockaddr *)&(this->_address),
-        sizeof(this->_address)))
+	if (bind(sock, (struct sockaddr *)&(address),
+        sizeof(address)))
 	{
 		std::cerr << "Could not bind socket" << std::endl;
     close(sock);
@@ -81,6 +73,49 @@ bool  Server::prepare(void)
 	return (true);
 }
 
+/*
+**  Obtain one listening socket per ServerConfig.
+**
+**  This function should be called for each listening socket.
+*/
+
+bool  Server::prepare(std::vector<ServerConfig> const & config)
+{
+  std::vector<ServerConfig>::const_iterator                   it;
+  std::set<std::size_t>                                       addedPorts;
+  std::map<int, std::vector<ServerConfig const *> >::iterator jt;
+  int                                                         sock;
+
+  for (it = config.begin(); it != config.end(); ++it)
+  {
+    if (addedPorts.insert(it->port).second == false)
+    {
+      /*
+      **  If port is already bound to a socket, add ServerConfig
+      **  as another value to that socket key in ServerConfig.
+      */
+      for (jt = this->_listeningSockets.begin();
+              jt != this->_listeningSockets.end(); ++jt)
+      {
+        if (it->port == jt->second.front()->port)
+          jt->second.push_back(&(*it));
+      }
+    }
+    else
+    {
+      /*
+      **  If port is not bound to a socket yet, set up socket
+      **  and add socket as key and ServerConfig as value in _listeningSockets.
+      */
+      if (!this->_initSocket(sock, it->port))
+        return (false);
+      this->_listeningSockets[sock].push_back(&(*it));
+      this->_connLen += 1;
+    }
+  }
+  return (true);
+}
+
 void  Server::_removeConnection(std::size_t index)
 {
   std::size_t i;
@@ -91,19 +126,127 @@ void  Server::_removeConnection(std::size_t index)
   return ;
 }
 
-bool  Server::_sendData(int socket, std::string & data)
-{
-  Response    res("hola.html");
-  std::string resData;
-  int         sent;
-  std::size_t totalSent;
+/*
+**  Provisional, need Request instance to get more info about the request.
+**
+**  A hash table could be implemented to avoid some file I/O and
+**  increase response efficiency.
+*/
 
-  (void)data; // Not reading data for now...
-  resData = res.get();
-  totalSent = 0;
-  while (totalSent != resData.length()) // send might not send all resData in one call
+void  Server::_getResponse(std::string & data, ConnectionData const & conn) const
+{
+  ServerConfig const *    serv = (*conn.portConfigs)[conn.serverIndex];
+  LocationConfig const *  loc = &serv->location[conn.locationIndex];
+  std::string             fileName;
+  std::ifstream           file;
+
+  fileName = loc->root + '/' + loc->default_file;
+  file.open(fileName.c_str());
+  if (file.is_open())
+    file.close();
+  else
+    fileName = serv->not_found_page;
+  data = Response(fileName).get();
+  return ;
+}
+
+void  Server::_matchLocation(std::vector<LocationConfig> const & locations,
+                              std::size_t & index, std::string const & reqUri)
+{
+  std::size_t                                 uriLen;
+  std::vector<LocationConfig>::const_iterator it;
+  std::size_t                                 i;
+
+  uriLen = 0;
+  for (it = locations.begin(), i = 0; it != locations.end(); ++it, ++i)
   {
-    sent = send(socket, resData.c_str() + totalSent, resData.length() - totalSent, 0);
+    if (reqUri.find(it->uri) == 0)
+    {
+      if (reqUri.length() > uriLen)
+      {
+        uriLen = it->uri.length();
+        index = i;
+      }
+    }
+  }
+  if (uriLen == 0)
+    index = 0;
+  return ;
+}
+
+void  Server::_matchServer(std::vector<ServerConfig const *> & servers,
+                              std::size_t & index, std::string const & reqHost)
+{
+  int                                         match;
+  std::vector<ServerConfig const *>::iterator it;
+  std::size_t                                 i;
+
+  match = -1;
+  // Match server
+  for (it = servers.begin(), i = 0; it != servers.end(); ++it, ++i)
+  {
+    if ((*it)->server_name.empty() && match == -1)
+    {
+      index = i;
+      match = 0;
+    }
+    else if ((*it)->server_name.count(reqHost))
+    {
+      index = i;
+      match = 1;
+      break ;
+    }
+  }
+  if (match == -1)
+    index = 0;
+  return ;
+}
+
+// Provisional
+
+void  Server::_matchConfig(int socket)
+{
+  ConnectionData *  sockData = &this->_connectionSockets[socket];
+  std::size_t       needle;
+  std::string       host;
+  std::string       uri;
+
+  needle = sockData->dataIn.find("Host:") + 6;
+  host = sockData->dataIn.substr(needle,
+          sockData->dataIn.find("\n", needle) - needle);
+  this->_matchServer(*(sockData->portConfigs),
+                        sockData->serverIndex, host);
+  needle = sockData->dataIn.find(" ") + 1;
+  uri = sockData->dataIn.substr(needle,
+          sockData->dataIn.find(" ", needle) - needle);
+  this->_matchLocation((*sockData->portConfigs)[sockData->serverIndex]->location,
+                        sockData->locationIndex, uri);
+}
+
+bool  Server::_sendData(int socket)
+{
+  ConnectionData *  sockData = &this->_connectionSockets[socket];
+  std::string &     dataOut = sockData->dataOut;
+  int               sent;
+  std::size_t       totalSent;
+
+  /*
+  **  This is provisional. Need to parse the request, check the location
+  **  configuration properly, and search in the root directory of
+  **  the location for the file
+  */
+  this->_matchConfig(socket);
+  this->_getResponse(dataOut, *sockData);
+  totalSent = 0;
+  /*
+  **  DETERMINE IF INSTEAD OF A LOOP THAT BLOCKS UNTIL THE ENTIRE DATA IS SENT,
+  **  IS MORE EFFICIENT TO SEND AGAIN TO poll AND CHECK AGAIN FOR POLLOUT UNTIL
+  **  THE ENTIRE DATA IS SENT.
+  */
+  while (totalSent != dataOut.length()) // send might not send all dataOut in one call
+  {
+    sent = send(socket, dataOut.c_str() + totalSent,
+                  dataOut.length() - totalSent, 0);
     if (sent < 0)
     {
       std::cout << "Could not send data to client." << std::endl;
@@ -114,13 +257,18 @@ bool  Server::_sendData(int socket, std::string & data)
   return (true);
 }
 
-bool  Server::_receiveData(int socket, std::string & data)
+bool  Server::_receiveData(int socket)
 {
   std::size_t const buffLen = 500;
   char              buff[buffLen + 1];
   int               len;
 
   std::fill(buff, buff + buffLen + 1, 0);
+  /*
+  **  DETERMINE IF INSTEAD OF A LOOP THAT BLOCKS UNTIL THE ENTIRE
+  **  DATA IS RECEIVED, IS MORE EFFICIENT TO SEND AGAIN TO poll
+  **  AND CHECK AGAIN FOR POLLIN UNTIL THE ENTIRE DATA IS RECEIVED.
+  */
   while (1)
   {
     len = recv(socket, buff, buffLen, 0);
@@ -131,7 +279,7 @@ bool  Server::_receiveData(int socket, std::string & data)
     }
     if (len < 0)
       break ;
-    data.append(buff, len);
+    this->_connectionSockets[socket].dataIn.append(buff, len);
     std::fill(buff, buff + buffLen, 0);
   }
   return (true);
@@ -151,12 +299,21 @@ void  Server::_increaseConnCap(void)
 
 bool  Server::_acceptConn(int listenSocket)
 {
-  int newConn;
+  std::vector< ServerConfig const * > * configs;
+  struct sockaddr_in                    address;
+  socklen_t									            addrLen;
+  int                                   newConn;
 
+  /*
+  **  Get configs vector associated to listeningSocket in advance
+  **  to avoid repeated searches in case more than one connection
+  **  has been received through this port.
+  */
+  configs = &this->_listeningSockets[listenSocket];
+  addrLen = sizeof(address);
   while (true)
   {
-    newConn = accept(listenSocket, (struct sockaddr *)&this->_address,
-                      (socklen_t *)&this->_addrlen);
+    newConn = accept(listenSocket, (struct sockaddr *)&address, &addrLen);
     if (newConn < 0)
     {
       if (errno != EWOULDBLOCK)
@@ -177,6 +334,11 @@ bool  Server::_acceptConn(int listenSocket)
     this->_connections[this->_connLen].fd = newConn;
     this->_connections[this->_connLen].events = POLLIN;
     this->_connLen += 1;
+    /*
+    **  Add new connection socket as key in _connectionSockets
+    **  and configs vector as value.
+    */
+    this->_connectionSockets[newConn].portConfigs = configs;
   }
   return (true);
 }
@@ -189,41 +351,48 @@ bool  Server::_acceptConn(int listenSocket)
 */
 bool  Server::_handleEvent(std::size_t index)
 {
-  int         socket;
-  std::string data;
-  bool        ok;
+  int   socket;
+  bool  ok;
 
   ok = true;
   socket = this->_connections[index].fd;
-  if (std::find(this->_sockets.begin(), this->_sockets.end(),
-        socket) != this->_sockets.end())
+  if (this->_listeningSockets.count(socket))
   {
     // New client/s connected to one of listening sockets
     if (!this->_acceptConn(socket))
       return (false);
   }
+  else if (this->_connections[index].revents & POLLIN)
+  {
+    // Connected client socket is ready to read without blocking
+    if (!this->_receiveData(socket))
+      return (false);
+    std::cout << "Data received !!!!!\n\n";
+    std::cout << this->_connectionSockets[socket].dataIn << std::endl;
+    this->_connections[index].events = POLLOUT;
+  }
   else
   {
-    // Connected client socket is ready to read
-    if (!this->_receiveData(socket, data))
-      return (false);
-    std::cout << "Data received !!!!!\n\n" << data << std::endl;
-    if (!this->_sendData(socket, data))
+    // Connected client socket is ready to write without blocking
+    if (!this->_sendData(socket))
       ok = false;
     close(socket);
     this->_removeConnection(index);
+    this->_connectionSockets.erase(socket);
   }
   return (ok);
 }
 
 void  Server::_monitorListenSocketEvents(void)
 {
-  std::size_t i;
+  std::map<int, std::vector<ServerConfig const *> >::iterator it;
+  std::size_t                                           i;
 
-  for (i = 0; i < this->_connLen; i++)
+  for (it = this->_listeningSockets.begin(), i = 0;
+          it != this->_listeningSockets.end(); ++it, ++i)
   {
-    this->_connections[i].fd = this->_sockets[i];
-    this->_connections[i].events = POLLIN; //Maybe also POLLOUT
+    this->_connections[i].fd = it->first;
+    this->_connections[i].events = POLLIN;
   }  
   return ;
 }
@@ -231,20 +400,21 @@ void  Server::_monitorListenSocketEvents(void)
 bool  Server::start(void)
 {
   std::size_t loopConnLen;
-  int         res;
+  int         numEvents;
   int         handlingCount;
 
   this->_monitorListenSocketEvents();
   while (true)
   {
     loopConnLen = this->_connLen; //Update number of monitored socket for next poll call
-    res = poll(this->_connections, loopConnLen, -1); // TIMEOUT -1 blocks until event is received
-    if (res < 0)
+    numEvents = poll(this->_connections, loopConnLen, -1); // TIMEOUT -1 blocks until event is received
+    if (numEvents < 0)
     {
       std::cerr << "poll() error" << std::endl;
       return (false);
     }
-    /*if (res == 0)
+    /* IF TIMEOUT IS NOT SET TO -1 ADD THIS BLOCK TO HANDLE TIMEOUTS
+    if (numEvents == 0)
     {
       std::cerr << "poll() timeout" << std::endl;
       continue ;
@@ -256,7 +426,7 @@ bool  Server::start(void)
         continue;
       if (!this->_handleEvent(i))
         break ;
-      if (++handlingCount == res) // To stop iterating when total events have been handled
+      if (++handlingCount == numEvents) // To stop iterating when total events have been handled
         break ;
     }    
   } 
