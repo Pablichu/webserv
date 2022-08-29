2fee0fca702aaae3ac44a62c77fb9715e8a9c6e5
onapoli <onapoli_dev@protonmail.com>
2022-08-29 09:50:19 +0200

    Add POST system test

---
+++ tests/postTest.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Need to run "make && make start" before executing this test script
+
+# dirname removes "/filename.extensension" from path
+RELATIVE_SCRIPT_PATH=$(dirname $BASH_SOURCE)
+UPLOAD_FILE_PATH="${RELATIVE_SCRIPT_PATH}/../sources/Config.cpp"
+TARGET_FILE_NAME="post_test.txt"
+TARGET_FILE_PATH="${RELATIVE_SCRIPT_PATH}/www/localhost/media/${TARGET_FILE_NAME}"
+SERVER="http://localhost:8080"
+REQUEST_URI="${SERVER}/gallery/media/${TARGET_FILE_NAME}"
+LOG_FILE_NAME="webserv_post.txt"
+LOG_FILE_PATH="/tmp/${LOG_FILE_NAME}"
+EXIT_CODE=""
+
+# POST big text file
+
+# -f makes curl return an error for response codes >= 400
+curl -v -f --data-binary @"${UPLOAD_FILE_PATH}" $REQUEST_URI &> $LOG_FILE_PATH
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ] || [ ! -f "$TARGET_FILE_PATH" ]
+then
+  echo "First POST request failed."
+  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+cat $LOG_FILE_PATH | grep "HTTP/1.1 201 Created" &> /dev/null
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "First POST request status code incorrect."
+  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+diff $UPLOAD_FILE_PATH $TARGET_FILE_PATH &> /dev/null
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "First POST request file contents are incorrect."
+  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+# POST to existing file to append
+
+curl -v -f --data-binary @"${UPLOAD_FILE_PATH}" $REQUEST_URI &> $LOG_FILE_PATH
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "Second POST request failed."
+  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+cat $LOG_FILE_PATH | grep "HTTP/1.1 200 OK" &> /dev/null
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "Second POST request status code incorrect."
+  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+# The appended file should have the content of the upload file duplicated.
+# So if diff determines that files are equal, it means that the file
+# was overwritten.
+
+diff $UPLOAD_FILE_PATH $TARGET_FILE_PATH &> /dev/null
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -eq "0" ]
+then
+  echo "Second POST request did not append."
+  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+# DELETE uploaded file
+
+curl -f -s -X DELETE $REQUEST_URI > /dev/null
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ] || [ -f "$TARGET_FILE_PATH" ]
+then
+  echo "DELETE request failed."
+  if [ -f "$TARGET_FILE_PATH" ]; then rm "$TARGET_FILE_PATH"; fi
+  exit $EXIT_CODE
+fi
+
+echo -e "\nPOST tests passed. Congrats!\n"
