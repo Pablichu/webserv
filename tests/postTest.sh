ba45f7a5d9e69a077e422b0785e7e211128a0d75
onapoli <onapoli_dev@protonmail.com>
2022-09-04 12:56:13 +0200

    Add chunked and image post tests

--- tests/postTest.sh
+++ tests/postTest.sh
@@ -4,11 +4,16 @@
 
 # dirname removes "/filename.extensension" from path
 RELATIVE_SCRIPT_PATH=$(dirname $BASH_SOURCE)
-UPLOAD_FILE_PATH="${RELATIVE_SCRIPT_PATH}/../sources/Config.cpp"
-TARGET_FILE_NAME="post_test.txt"
-TARGET_FILE_PATH="${RELATIVE_SCRIPT_PATH}/www/localhost/media/${TARGET_FILE_NAME}"
+UPLOAD_TXT_FILE_PATH="${RELATIVE_SCRIPT_PATH}/../sources/Config.cpp"
+DOWNLOAD_IMG_URI="https://ia800304.us.archive.org/7/items/SPD-SLRSY-1826/rocketry_21.jpg"
+DOWNLOAD_IMG_FILE_PATH="${RELATIVE_SCRIPT_PATH}/../post_test.jpg"
+TARGET_TXT_FILE_NAME="post_test.txt"
+TARGET_TXT_FILE_PATH="${RELATIVE_SCRIPT_PATH}/www/localhost/media/${TARGET_TXT_FILE_NAME}"
+TARGET_IMG_FILE_NAME="post_test.jpg"
+TARGET_IMG_FILE_PATH="${RELATIVE_SCRIPT_PATH}/www/localhost/media/${TARGET_IMG_FILE_NAME}"
 SERVER="http://localhost:8080"
-REQUEST_URI="${SERVER}/gallery/media/${TARGET_FILE_NAME}"
+TXT_REQUEST_URI="${SERVER}/gallery/media/${TARGET_TXT_FILE_NAME}"
+IMG_REQUEST_URI="${SERVER}/gallery/media/${TARGET_IMG_FILE_NAME}"
 LOG_FILE_NAME="webserv_post.txt"
 LOG_FILE_PATH="/tmp/${LOG_FILE_NAME}"
 EXIT_CODE=""
@@ -19,18 +24,18 @@ EXIT_CODE=""
 #
 # &> is not portable. 2>&1 redirects stderr to stdout
 #
-curl -v -f --data-binary @"${UPLOAD_FILE_PATH}" $REQUEST_URI \
+curl -v -f --data-binary @"${UPLOAD_TXT_FILE_PATH}" $TXT_REQUEST_URI \
   > $LOG_FILE_PATH 2>&1
 
 EXIT_CODE="$?"
 
-if [ "$EXIT_CODE" -ne "0" ] || [ ! -f "$TARGET_FILE_PATH" ]
+if [ "$EXIT_CODE" -ne "0" ] || [ ! -f "$TARGET_TXT_FILE_PATH" ]
 then
   if [ "$EXIT_CODE" -ne "22" ]; then
     echo "Run make && make start before executing this test script."
   else
     echo "First POST request failed."; fi
-  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
   exit $EXIT_CODE
 fi
 
@@ -41,24 +46,24 @@ EXIT_CODE="$?"
 if [ "$EXIT_CODE" -ne "0" ]
 then
   echo "First POST request status code incorrect."
-  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
   exit $EXIT_CODE
 fi
 
-diff $UPLOAD_FILE_PATH $TARGET_FILE_PATH > /dev/null 2>&1
+diff $UPLOAD_TXT_FILE_PATH $TARGET_TXT_FILE_PATH > /dev/null 2>&1
 
 EXIT_CODE="$?"
 
 if [ "$EXIT_CODE" -ne "0" ]
 then
   echo "First POST request file contents are incorrect."
-  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
   exit $EXIT_CODE
 fi
 
 # POST to existing file to append
 
-curl -v -f --data-binary @"${UPLOAD_FILE_PATH}" $REQUEST_URI \
+curl -v -f --data-binary @"${UPLOAD_TXT_FILE_PATH}" $TXT_REQUEST_URI \
   > $LOG_FILE_PATH 2>&1
 
 EXIT_CODE="$?"
@@ -66,7 +71,7 @@ EXIT_CODE="$?"
 if [ "$EXIT_CODE" -ne "0" ]
 then
   echo "Second POST request failed."
-  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
   exit $EXIT_CODE
 fi
 
@@ -77,7 +82,7 @@ EXIT_CODE="$?"
 if [ "$EXIT_CODE" -ne "0" ]
 then
   echo "Second POST request status code incorrect."
-  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
   exit $EXIT_CODE
 fi
 
@@ -85,27 +90,127 @@ fi
 # So if diff determines that files are equal, it means that the file
 # was overwritten.
 
-diff $UPLOAD_FILE_PATH $TARGET_FILE_PATH > /dev/null 2>&1
+diff $UPLOAD_TXT_FILE_PATH $TARGET_TXT_FILE_PATH > /dev/null 2>&1
 
 EXIT_CODE="$?"
 
 if [ "$EXIT_CODE" -eq "0" ]
 then
   echo "Second POST request did not append."
-  if [ -f "$TARGET_FILE_PATH" ]; then rm $TARGET_FILE_PATH; fi
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
   exit $EXIT_CODE
 fi
 
 # DELETE uploaded file
 
-curl -f -s -X DELETE $REQUEST_URI > /dev/null 2>&1
+curl -f -s -X DELETE $TXT_REQUEST_URI > /dev/null 2>&1
 
 EXIT_CODE="$?"
 
-if [ "$EXIT_CODE" -ne "0" ] || [ -f "$TARGET_FILE_PATH" ]
+if [ "$EXIT_CODE" -ne "0" ] || [ -f "$TARGET_TXT_FILE_PATH" ]
 then
   echo "DELETE request failed."
-  if [ -f "$TARGET_FILE_PATH" ]; then rm "$TARGET_FILE_PATH"; fi
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm "$TARGET_TXT_FILE_PATH"; fi
+  exit $EXIT_CODE
+fi
+
+# POST big text file in chunks
+
+curl -v -f --data-binary @"${UPLOAD_TXT_FILE_PATH}" $TXT_REQUEST_URI \
+  -H "Transfer-Encoding: chunked" > $LOG_FILE_PATH 2>&1
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ] || [ ! -f "$TARGET_TXT_FILE_PATH" ]
+then
+  echo "Chunked POST request failed."; fi
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+cat $LOG_FILE_PATH | grep "HTTP/1.1 201 Created" > /dev/null 2>&1
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "Chunked POST request status code incorrect."
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+diff $UPLOAD_TXT_FILE_PATH $TARGET_TXT_FILE_PATH > /dev/null 2>&1
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "Chunked POST request file contents are incorrect."
+  if [ -f "$TARGET_TXT_FILE_PATH" ]; then rm $TARGET_TXT_FILE_PATH; fi
+  exit $EXIT_CODE
+fi
+
+# Download image
+
+curl -L $DOWNLOAD_IMG_URI -o $DOWNLOAD_IMG_FILE_PATH > /dev/null 2>&1
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "Image download failed. Check if url is still valid."
+  exit $EXIT_CODE
+fi
+
+# Upload image
+
+curl -v -f --data-binary @"$DOWNLOAD_IMG_FILE_PATH" $IMG_REQUEST_URI \
+  -H "Content-Type: image/jpeg" > $LOG_FILE_PATH 2>&1
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ] || [ ! -f "$TARGET_IMG_FILE_PATH" ]
+then
+  echo "Image upload failed."
+  rm $DOWNLOAD_IMG_FILE_PATH
+  exit $EXIT_CODE
+fi
+
+cat $LOG_FILE_PATH | grep "HTTP/1.1 201 Created" > /dev/null 2>&1
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "Image POST request status code incorrect."
+  if [ -f "$TARGET_IMG_FILE_PATH" ]; then rm $TARGET_IMG_FILE_PATH; fi
+  rm $DOWNLOAD_IMG_FILE_PATH
+  exit $EXIT_CODE
+fi
+
+diff $DOWNLOAD_IMG_FILE_PATH $TARGET_IMG_FILE_PATH > /dev/null 2>&1
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ]
+then
+  echo "Image POST file contents are incorrect."
+  if [ -f "$TARGET_IMG_FILE_PATH" ]; then rm $TARGET_IMG_FILE_PATH; fi
+  rm $DOWNLOAD_IMG_FILE_PATH
+  exit $EXIT_CODE
+fi
+
+# DELETE uploaded image
+
+curl -f -s -X DELETE $IMG_REQUEST_URI > /dev/null 2>&1
+
+EXIT_CODE="$?"
+
+if [ "$EXIT_CODE" -ne "0" ] || [ -f "$TARGET_IMG_FILE_PATH" ]
+then
+  echo "DELETE request failed."
+  if [ -f "$TARGET_IMG_FILE_PATH" ]; then rm "$TARGET_IMG_FILE_PATH"; fi
+  rm $DOWNLOAD_IMG_FILE_PATH
   exit $EXIT_CODE
 fi
 
