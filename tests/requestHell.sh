7108b2a8089f8fae44bc1d5aeac86cab57767b30
onapoli <onapoli_dev@protonmail.com>
2022-09-23 20:20:21 +0200

    Add POST requests to requestHell script

--- tests/requestHell.sh
+++ tests/requestHell.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+RELATIVE_SCRIPT_PATH=$(dirname $BASH_SOURCE)
+UPLOAD_TXT_FILE_PATH="${RELATIVE_SCRIPT_PATH}/../sources/Config.cpp"
+SERVER="http://localhost:8080"
+TXT_REQUEST_URI="${SERVER}/gallery/media"
+RAND_VALUE=""
+TEST_FILE_NAME=""
+
 nc -z localhost 8080
 
 if [ "$?" -ne "0" ]
@@ -23,6 +30,14 @@ do
   curl localhost:3000/gallery &
   curl -X GET localhost:9000/reply.py -H "Content-Type: text/plain" \
         -d "Hello Everyone!"
+  RAND_VALUE=$(openssl rand -hex 10)
+  TEST_FILE_NAME="$RAND_VALUE.txt"
+  curl -f --data-binary @"${UPLOAD_TXT_FILE_PATH}" "$TXT_REQUEST_URI/$TEST_FILE_NAME" \
+    -H "Content-Type: text/plain"
+  curl -f -s -X DELETE "$TXT_REQUEST_URI/$TEST_FILE_NAME"
+  curl -f --data-binary @"${UPLOAD_TXT_FILE_PATH}" "$TXT_REQUEST_URI/$TEST_FILE_NAME" \
+    -H "Content-Type: text/plain" -H "Transfer-Encoding: chunked"
+  curl -f -s -X DELETE "$TXT_REQUEST_URI/$TEST_FILE_NAME"
   # There is no need to call wait for this use case
   sleep 0.09 #seconds
 done
